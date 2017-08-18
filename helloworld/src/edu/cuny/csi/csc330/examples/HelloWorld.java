9bef2c37b17bb4f3aabe80ead21efe465cabec0c
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2017-08-17 22:33:16 -0400

    Lab 01 initial commit.

---
+++ helloworld/src/edu/cuny/csi/csc330/examples/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * HelloWorld.java - Simple Java Hello World program
+ * @author Suresh Melvin Sigera
+ * @version 1.0
+ *
+/**
+
+
+/**
+ * THIS IS A COMMENT BLOCK
+ * CUNY/CSI CSC 330 WEEK 1 - EXAMPLE 1
+ *
+ **/
+
+package edu.cuny.csi.csc330.examples;
+
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello, World");
+    }
+}
