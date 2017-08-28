d5ae9a3e6809b34a3ad1ed96a7e32b156765e6dc
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2017-08-28 19:41:18 -0400

    Source code update.

--- helloworld/src/edu/cuny/csi/csc330/examples/HelloWorld.java
+++ helloworld/src/edu/cuny/csi/csc330/examples/HelloWorld.java
@@ -1,13 +1,24 @@
 /**
- * THIS IS A COMMENT BLOCK
- * CUNY/CSI CSC 330 WEEK 1 - EXAMPLE 1
- *
- **/
+ Course: 330-E001(12968)
+ Filename : lab1.cpp
+ Purpose: Print Hello World, and current system time.
+ @author Suresh Melvin Sigera
+ @date 08/10/2017
+ */
 
 package edu.cuny.csi.csc330.examples;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String args[]){
+
         System.out.println("Hello, World");
+
+        Date now = Calendar.getInstance().getTime();
+
+        System.out.println("The current date/time is: " + Calendar.getInstance().getTime() );
+
     }
 }
