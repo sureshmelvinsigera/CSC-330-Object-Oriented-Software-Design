efc7db55de96c9131a1a02ef3b17200969a5a536
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2017-08-28 20:24:58 -0400

    Source code update.

--- helloworld/src/edu/cuny/csi/csc330/examples/HelloWorld.java
+++ helloworld/src/edu/cuny/csi/csc330/examples/HelloWorld.java
@@ -1,7 +1,7 @@
 /**
  * Course: 330-E001(12968)
  * Filename : lab1.cpp
- * Purpose: Print Hello World, and current system time.
+ * Purpose: Print Hello World, and current system time to the standard output.
  * @author Suresh Melvin Sigera
  * @date 08/10/2017
  */
@@ -14,11 +14,14 @@ import java.util.Date;
 public class HelloWorld {
     public static void main(String args[]) {
 
+        //print message to the standard output
         System.out.println("Hello, World");
 
         Date now = Calendar.getInstance().getTime();
 
-        System.out.println("The current date/time is: " + Calendar.getInstance().getTime());
+        //System.out.println("The current date/time is: " + Calendar.getInstance().getTime());
 
+        //print current system time
+        System.out.println("The current date/time is: " + now);
     }
-}
+}//end of class HelloWorld
